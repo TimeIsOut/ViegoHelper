2bb24d8a2fd27c02c2f7a8190158448747790986
TimeIsOut <y0u-g0t-hack3d@yandex.ru>
2022-02-17 15:18:45 +0300

    feat: add discord bot base

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-class Testing:
-    def __init__(self):
-        self.test_text = "Hello World!"
-    
-    def print(self):
-        print(self.test_text)
+import os
+import discord
+from discord import commands
+
+BOT = commands.Bot(command_prefix="!")
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+@BOT.event
+async def on_ready():
+    print(f"Logged in as {BOT.user.name}({BOT.user.id})")
+
+@BOT.command()
+async def ping(ctx):
+    await ctx.send("pong")
+
+
+if __name__ == "__main__":
+    BOT.run(TOKEN)

97c634943e9b5759ca106665071df33b80d56b41
TimeIsOut <y0u-g0t-hack3d@yandex.ru>
2022-02-22 20:02:29 +0300

    feat: add menu command (in development)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
-from variables import TOKEN
+from discord import Embed
+from discord_components import Button, ButtonStyle
+from variables import TOKEN, DATA_PATH
 
 BOT = commands.Bot(command_prefix="!")
 
@@ -7,9 +9,23 @@ BOT = commands.Bot(command_prefix="!")
 async def on_ready():
     print(f"Logged in as {BOT.user.name}({BOT.user.id})")
 
+@BOT.event
+async def on_button_click(interaction):
+    BOT.invoke(interaction.component.custom_id)
+    
+
 @BOT.command()
 async def ping(ctx):
     await ctx.send("pong")
+
+@BOT.command()
+async def menu(ctx):
+    embed = Embed()
+    embed.title = "Welcome to Viego Helper!"
+    embed.description = "Choose one of the commands to go!"
+    embed.set_thumbnail(f"{DATA_PATH}/viego_avatar.jpg")
+    await ctx.send(embed=embed, components=[[Button(label="Choose a random agent", style=ButtonStyle.gray, custom_id="random_agents"),
+                                             Button(label="All VALORANT agents", style=ButtonStyle.gray, custom_id="all_agents")]])
 
 
 if __name__ == "__main__":
--- variables.py
+++ variables.py
@@ -4,3 +4,4 @@ from os import getenv
 VAL_KEY = getenv('VALORANT_KEY')
 LOL_KEY = getenv('LOL_KEY')
 TOKEN = getenv("DISCORD_TOKEN")
+DATA_PATH = "https://raw.githubusercontent.com/TimeIsOut/ViegoHelperData/master"
